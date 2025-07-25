52a8b07df65db895a9bb20625655416557ca8260
jagadish valluri <jagadishvalluri64@gmail.com>
2025-07-25 14:21:47 +0530

    i added comments

--- src/main/java/basic/helloworld.java
+++ src/main/java/basic/helloworld.java
@@ -4,7 +4,8 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello Jagadish!!!");
+	     System.out.println("Hello Jagadish!!!");
+	     // added comments to check github actions
 	}
 
 }
